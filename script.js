6f6cd545d64a29e293f8e0c10714f4ee9118402f
AsifAliSoftage <153803870+AsifAliSoftage@users.noreply.github.com>
2023-12-20 10:37:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('clickMe').addEventListener('click',function(){
+  const button=document.getElementById('clickMe');
+  if(button.innerText === 'Click Me!'){
+     button.innerText = "Clicked!;
+} else {
+button.innerText="Click Me!";
+}
+}
+)
+
+                                                    
